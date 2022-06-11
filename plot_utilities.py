5fe0c92150bbe1f4461826449c7c3458c962f44e
alex.fazeli <alex.a.fazeli@gmail.com>
2022-06-11 19:46:59 -0400

    try

--- plot_utilities.py
+++ plot_utilities.py
@@ -10,4 +10,5 @@ def plot_mechanism(x_e, y_e, teta, L):
         plt.plot([0, L[0] * np.cos(teta[0, i]), L[0] * np.cos(teta[0, i])+L[1]*np.cos(teta[0, i]+teta[1, i]), L[0] * np.cos(teta[0, i]) + L[1]*np.cos(teta[0, i]+teta[1, i]) + L[2]*np.cos(teta[0, i]+teta[1, i]+teta[2, i])], [0, L[0] * np.sin(teta[0, i]), L[0] * np.sin(teta[0, i])+L[1]*np.sin(teta[0, i]+teta[1, i]), L[0] * np.sin(teta[0, i]) + L[1]*np.sin(teta[0, i]+teta[1, i]) + L[2]*np.sin(teta[0, i]+teta[1, i]+teta[2, i])])
         plt.pause(1)
     plt.plot(x_e, y_e)
+
     plt.show()
