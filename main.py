a27e7901fa235b0d8903b42e9bb961ade5dd980c
alex.fazeli <alex.a.fazeli@gmail.com>
2022-06-12 02:11:59 -0400

    add teta_dot calculation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
-from robot_analyzer import end_effector_trajactory_creator, joins_angles
+from robot_analyzer import end_effector_trajactory_creator, joins_angles, end_effector_speed_calculator, jacobian_calculator
 from plot_utilities import plot_mechanism
+import numpy as np
+from numpy.linalg import inv
+
 
 with open(r"C:\Users\amirf\PycharmProjects\Manipulator2\config.json") as f:
     config_param = json.load(f)
@@ -8,14 +11,23 @@ pi = config_param['initial_point']
 pf = config_param['final_point']
 N = config_param['N']
 L = config_param['L']
+time = config_param['time']
 
 [x_e, y_e, phi_e] = end_effector_trajactory_creator(pi, pf, N)
 teta = joins_angles(x_e, y_e, phi_e, L)
 
 plot_mechanism(x_e, y_e, teta, L)
 
+speed = end_effector_speed_calculator(pi, pf, time)
 
+#teta_dot = np.zeros(np.shape(teta))
+teta_dot = [[],[],[]]
 
+for i in range(N+1):
+    J = jacobian_calculator(teta[0, i], teta[1, i], teta[2, i], L)
+    teta_dot = np.c_[teta_dot,  np.matmul(inv(J), speed)]
+
+output = np.r_[teta, teta_dot]
 
 
 # Press the green button in the gutter to run the script.
--- robot_analyzer.py
+++ robot_analyzer.py
@@ -26,5 +26,33 @@ def joins_angles(x_e, y_e, phi_e, L):
     teta[0, :] = np.arctan2(joint_2_y, joint_2_x) - np.arctan2(k2, k1)
     teta[2, :] = phi_e - teta[0, :] - teta[1, :]
 
-
     return teta
+
+def end_effector_speed_calculator(pt_i, pt_f, T):
+    speed_vector = np.zeros((3, 1))
+    speed_vector[0] = (pt_f[0] - pt_i[0]) / T
+    speed_vector[1] = (pt_f[1] - pt_i[1]) / T
+    speed_vector[2] = (pt_f[2] - pt_i[2]) / T
+
+
+    return speed_vector
+
+
+def jacobian_calculator(Teta_1, Teta_2, Teta_3, L):
+
+
+    a11 = -L[0]*np.sin(Teta_1) - L[1]*np.sin(Teta_1+Teta_2) - L[2]*np.sin(Teta_1+Teta_2+Teta_3)
+    a12 = -L[1]*np.sin(Teta_1+Teta_2) - L[2]*np.sin(Teta_1+Teta_2+Teta_3)
+    a13 = -L[2]*np.sin(Teta_1+Teta_2+Teta_3)
+
+    a21 = L[0]*np.cos(Teta_1) + L[1]*np.cos(Teta_1+Teta_2) + L[2]*np.cos(Teta_1+Teta_2+Teta_3)
+    a22 = L[1]*np.cos(Teta_1+Teta_2) + L[2]*np.cos(Teta_1+Teta_2+Teta_3)
+    a23 = L[2]*np.cos(Teta_1+Teta_2+Teta_3)
+
+    a31 = 1
+    a32 = 1
+    a33 = 1
+
+    J = np.array([[a11, a12, a13], [a21, a22, a23], [a31, a32, a33]])
+
+    return J
